22888ffc4a5b8e1664e80a4f8f34620ec4963e9c
David Davis <davisd50@gmail.com>
2016-01-22 14:58:51 -0500

    add db query, result, and value interfaces

--- sparc/db/interfaces.py
+++ sparc/db/interfaces.py
@@ -1,6 +1,7 @@
 from zope.interface import Interface
 from zope import schema
 
+#DB Query
 class IQuery(Interface):
     """A database query"""
     query = schema.Text(
@@ -8,3 +9,33 @@ class IQuery(Interface):
             description=(u"Identifies the network address of the impacted host"),
             required = True
             )
+
+#DB Query Results
+class IQueryResultSet(IQuery):
+    """A database query with results"""
+    def __iter__():
+        """Iterator of IResult objects"""
+
+class IResult(Interface):
+    """A single database query result (i.e. a row)"""
+
+class ITableResult(IResult):
+    """A single database query result represented as a Python dictionary whose
+    key values are instances of ITableResultMultiValue or ITableResultValue
+
+    Sample implementation:
+        >>> from zope.interface import implements
+        >>> class MyColumnarResult(dict):
+        ...     implements(IColumnarResult)
+    """
+
+#DB Query Result Values
+class ITableResultValue(Interface):
+    """A value from a database query result (i.e. a table cell)"""
+    def __unicode__():
+        """Return unicode string representation of value"""
+
+class ITableResultMultiValue(Interface):
+    """A value for a ITableResult key that allows for multi-value fields"""
+    def __iter__():
+        """Iterator of unicode capable values"""
