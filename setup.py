d4f7fab140bd51905ea05490073bf9725bad27b0
David Davis <davisd50@gmail.com>
2016-03-18 11:24:17 -0400

    add sparc.testing dependency

--- setup.py
+++ setup.py
@@ -41,6 +41,9 @@ setup(name='sparc.db',
           'sparc.i18n'
           # -*- Extra requirements: -*-
       ],
+      tests_require=[
+          'sparc.testing'
+      ],
       entry_points="""
       # -*- Entry points: -*-
       """,
