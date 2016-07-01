2e091158782fcb5d47184fa78b9fac064b4301c3
David Davis <davisd50@gmail.com>
2016-07-01 14:00:11 -0400

    remove conn info

--- sparc/db/splunk/__init__.py
+++ sparc/db/splunk/__init__.py
@@ -2,7 +2,15 @@ from interfaces import ISplunkConnectionInfo
 from interfaces import ISplunkQuery
 from interfaces import ISplunkResultsStream
 from interfaces import ISplunkJob
+from interfaces import ISPlunkKVCollectionIdentifier
 from interfaces import ISplunkKVCollectionSchema
 from interfaces import ISplunkSavedSearch
 from interfaces import ISplunkSavedSearches
 from interfaces import ISplunkSavedSearchQueryFilter
+
+# XML name spaces for REST API ElementTree searching
+xml_ns = {
+          'atom': 'http://www.w3.org/2005/Atom',
+          's': 'http://dev.splunk.com/ns/rest',
+          'opensearch': 'http://a9.com/-/spec/opensearch/1.1/'
+        }
