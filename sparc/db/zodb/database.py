c86f34be25b700e8a8fd0c080aed15934339d4ca
David Davis <davisd50@gmail.com>
2016-08-17 08:05:49 -0400

    remove redundant import

--- sparc/db/zodb/database.py
+++ sparc/db/zodb/database.py
@@ -5,7 +5,6 @@ from zope.component.factory import Factory
 from zope.interface import alsoProvides
 from zope.interface import implements
 from interfaces import IZODBDatabase
-from zope.component._api import createObject
 from sparc.configuration.xml import IAppElementTreeConfig
 
 class zodbDatabaseFactoryHelper(object):
